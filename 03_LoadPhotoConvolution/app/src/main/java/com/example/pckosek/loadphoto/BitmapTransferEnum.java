65a28ccb30d4a85813ab34f8f700ab7d6c764f72
pckosek <pckosek@fcps.edu>
2018-12-19 09:53:32 -0500

    conv

---
+++ 03_LoadPhotoConvolution/app/src/main/java/com/example/pckosek/loadphoto/BitmapTransferEnum.java
@@ -0,0 +1,23 @@
+package com.example.pckosek.loadphoto;
+
+import android.graphics.Bitmap;
+
+enum BitmapTransferEnum {
+    INSTANCE;
+
+    private Bitmap mBitmap;
+    public final static String KEY = "BitmapTransferEnum";
+
+
+    public static void setData(final Bitmap b) {
+        INSTANCE.mBitmap = b;
+    }
+
+    public static Bitmap getData() {
+        Bitmap retBitmap = INSTANCE.mBitmap;
+        Bitmap outBitmap = retBitmap.copy(retBitmap.getConfig(), true);
+//        INSTANCE.mBitmap.recycle();
+//        INSTANCE.mBitmap = null;
+        return outBitmap;
+    }
+}
